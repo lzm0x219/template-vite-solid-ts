5b9de22b1028569ac7de46fa3b1bd4a2f3e5a944
0x219 <lzm0x219@gmail.com>
2022-12-07 14:48:07 +0800

    Remove vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import paths from "vite-tsconfig-paths";
 import mkcert from "vite-plugin-mkcert";
+import { resolve } from "node:path";
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src"),
+      },
+    ],
+  },
   server: {
     https: true,
     port: 4300,
   },
-  plugins: [solid(), paths(), mkcert()],
+  plugins: [solid(), mkcert()],
   build: {
     target: "esnext",
-    polyfillDynamicImport: false,
     rollupOptions: {
       output: {
         manualChunks: {
